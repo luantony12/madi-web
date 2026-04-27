b767d39fe80178145f084af965c7a9041cb83c4f
luantony12 <luantony_12@gmail.com>
2026-04-27 01:03:36 -0500

    FOOTER

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,7 +5,7 @@
       <div class="tagline">Construyendo el futuro desde 2004</div>
       <p>Empresa líder en proyectos de diseño, construcción residencial, comercial e industrial con presencia en todo el país y estándares internacionales de calidad.</p>
       <div class="social-links" style="margin-top:24px;">
-        <a href="#" class="social-link"><i class="bi bi-instagram"></i></a>
+        <a href="https://www.instagram.com/disenodeinteriorescusco?igsh=MWlpY2JpajA2M2Y2aA==" class="social-link"><i class="bi bi-instagram"></i></a>
         <a href="#" class="social-link"><i class="bi bi-facebook"></i></a>
         <a href="#" class="social-link"><i class="bi bi-linkedin"></i></a>
         <a href="#" class="social-link"><i class="bi bi-youtube"></i></a>
@@ -15,11 +15,10 @@
     <div class="footer-col">
       <h4>Servicios</h4>
       <ul>
-        <li><a href="#servicios">Construcción Residencial</a></li>
-        <li><a href="#servicios">Edificios Comerciales</a></li>
-        <li><a href="#servicios">Obras Industriales</a></li>
-        <li><a href="#servicios">Remodelaciones</a></li>
-        <li><a href="#servicios">Gerencia de Proyectos</a></li>
+        <li><a href="#servicios">Arquitectura e Ingeniería</a></li>
+        <li><a href="#servicios">Mantenimiento y Remodelaciones</a></li>
+        <li><a href="#servicios">Saneamiento Físico y Legal</a></li>
+        <li><a href="#servicios">Otros Servicios</a></li>
       </ul>
     </div>
 
@@ -36,7 +35,8 @@
   </div>
 
   <div class="footer-bottom">
-    <p>© {{ date('Y') }} MADI Soluciones. Todos los derechos reservados.</p>
+    <p>{{ date('Y') }} MADI Soluciones.</p>
+    <p>© by A.N.C.O. - Todos los derechos reservados.</p>
     <p style="font-size:12px; color:var(--gray);">RUC: 2000099999 &nbsp;|&nbsp; Licencia de construcción N° 00456</p>
   </div>
 </footer>
